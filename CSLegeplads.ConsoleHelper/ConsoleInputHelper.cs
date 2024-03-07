b579000dbaadeabbbdf9b8ecffb08754a6e94a96
Steffen Jim Clausen <Steffen.Jim.Clausen@nordzucker.com>
2024-03-07 08:24:29 +0100

    implement feature consolehelper

---
+++ CSLegeplads.ConsoleHelper/ConsoleInputHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLegeplads.ConsoleInputHelper
+{
+    public static class ConsoleInputHelper
+    {
+
+        public static int GetInt(String prompt, int default_value)
+        {
+            try
+            {
+                Console.WriteLine(prompt);
+                String input = Console.ReadLine();
+
+                if (input == null || input.Trim().Length == 0)
+                {
+                    return default_value;
+                }
+
+                return Int32.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                ConsoleInputException consoleInputException = new ConsoleInputException("Input error!", ex);
+                throw consoleInputException;
+            }
+        }
+
+        public static double GetDouble(String prompt, double default_value)
+        {
+            try
+            {
+                Console.WriteLine(prompt);
+
+                String input = Console.ReadLine();
+
+                if (input == null || input.Trim().Length == 0)
+                {
+                    return default_value;
+                }
+
+                return Double.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                ConsoleInputException consoleInputException = new ConsoleInputException("Input error!", ex);
+                throw consoleInputException;
+            }
+        }
+
+
+        public static bool GetBool(String prompt, bool default_value)
+        {
+            try
+            {
+                Console.WriteLine(prompt);
+
+                String input = Console.ReadLine();
+
+                if (input == null || input.Trim().Length == 0)
+                {
+                    return default_value;
+                }
+
+                return Boolean.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                ConsoleInputException consoleInputException = new ConsoleInputException("Input error!", ex);
+                throw consoleInputException;
+            }
+        }
+
+
+        public static DateTime GetDateTime(String prompt, DateTime default_value)
+        {
+            try
+            {
+                Console.WriteLine(prompt);
+
+                String input = Console.ReadLine();
+
+                if (input == null || input.Trim().Length == 0)
+                {
+                    return default_value;
+                }
+
+                return DateTime.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                ConsoleInputException consoleInputException = new ConsoleInputException("Input error!", ex);
+                throw consoleInputException;
+            }
+        }
+    }
+}
--- CSLegeplads.ConsoleTest/Program.cs
+++ CSLegeplads.ConsoleTest/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSLegeplads.ConsoleInputHelper;
+using System.Reflection.Metadata;
+
+try
+{
+    int a = ConsoleInputHelper.GetInt("Input number", 0);
+    Console.WriteLine($"User input: {a}");
+
+    double b = ConsoleInputHelper.GetDouble("Input double", 0.0);
+    Console.WriteLine($"User input: {b}");
+
+    bool c = ConsoleInputHelper.GetBool("Input bool", false);
+    Console.WriteLine($"User input: {c}");
+
+    DateTime d = ConsoleInputHelper.GetDateTime("Input datetime", DateTime.Now);
+    Console.WriteLine($"User input: {d}");
+}
+catch (ConsoleInputException ex)
+{
+    Console.WriteLine(ex.ToString());
+}
